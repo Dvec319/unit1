562b3a3cf6341cc2371bba15a6a8f035fb01fc6a
mdelvecchio <mdelvecchio3119@gmail.com>
2023-05-02 22:12:09 -0400

    verbal questions completed

---
+++ week2/w2d1_hw/solutions.js
@@ -0,0 +1,3 @@
+// A parameter is the name for the value you feed into a function, such as a number or a string or even another function.
+// An argument is the actual value that takes the place of the parameter when the function is called.\
+// Within a function, console.log will only print out the result to the terminal. We won't be able to do anything with it. When we use return, we are getting back the actual value from the function which we can store in a variable. Also, when the function hits return, it immediately stops and returns the value.
