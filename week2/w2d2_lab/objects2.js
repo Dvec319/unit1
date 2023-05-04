3518ba84091b33b56022f66042a98ab56214eb80
mdelvecchio <mdelvecchio3119@gmail.com>
2023-05-04 18:31:27 -0400

    more work on the labs

--- week2/w2d2_lab/objects2.js
+++ week2/w2d2_lab/objects2.js
@@ -31,33 +31,67 @@
 
 // Ogres
 
-const ogre = {
-    name: "Smasher",
-    hitPoints: 20,
-    weapon: "club",
-    damage: 5,
-    attack: function (enemy){
-        adventurer.hitPoints -= ogre.damage;
-        console.log(`${ogre.name} landed the attack! ${enemy.name} has ${enemy.hitPoints}HP left. `);
-    }
-};
-const adventurer = {
-    name: "Billy",
-    hitPoints: 40,
-    weapon: "sword",
-    damage: 10,
-    attack: function (enemy){
-        ogre.hitPoints -= adventurer.damage;
-	    console.log(`${adventurer.name} landed the attack! ${enemy.name} has ${enemy.hitPoints}HP left. `);
-    }
+// const ogre = {
+//     name: "Smasher",
+//     hitPoints: 20,
+//     weapon: "club",
+//     damage: 5,
+//     attack: function (enemy){
+//         adventurer.hitPoints -= ogre.damage;
+//         console.log(`${ogre.name} landed the attack! ${enemy.name} has ${enemy.hitPoints}HP left. `);
+//     }
+// };
+// const adventurer = {
+//     name: "Billy",
+//     hitPoints: 40,
+//     weapon: "sword",
+//     damage: 10,
+//     attack: function (enemy){
+//         ogre.hitPoints -= adventurer.damage;
+// 	    console.log(`${adventurer.name} landed the attack! ${enemy.name} has ${enemy.hitPoints}HP left. `);
+//     }
+// };
+
+// while ((adventurer.hitPoints > 0) && (ogre.hitPoints > 0)) {
+//     (adventurer.attack(ogre))
+//     if (ogre.hitPoints <= 0) {break}
+//     (ogre.attack(adventurer))
+// };
+
+// if (ogre.hitPoints <= 0) {
+//      console.log(`Good job ${adventurer.name}! You beat the ogre ${ogre.name}.`);
+// }
+
+const cat1 = {
+    name: "Pudding",
+    breed:"American Bobtail",
+    age: 5
 };
 
-while ((adventurer.hitPoints > 0) && (ogre.hitPoints > 0)) {
-    (adventurer.attack(ogre))
-    if (ogre.hitPoints <= 0) {break}
-    (ogre.attack(adventurer))
+console.log(cat1.age);
+console.log(cat1.breed);
+
+const cat2 = {
+    name: "Gabagool",
+    breed: "Maine Coon",
+    age: 3
 };
 
-if (ogre.hitPoints <= 0) {
-     console.log(`Good job ${adventurer.name}! You beat the ogre ${ogre.name}.`);
-}
+// const combineCats = (mama, papa) => {
+//     console.log(mama, papa)
+// }
+
+// combineCats(cat1, cat2);
+
+const combineCats = (mama, papa) => {
+    const cat3 = {
+        name: mama.name + papa.name,
+        breed: mama.breed + "-" + papa.breed,
+        age: 1
+    }
+    return cat3
+};
+
+console.log(combineCats(cat1, cat2));
+console.log(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)));
+console.log(combineCats(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)), combineCats(cat1, cat2)));
