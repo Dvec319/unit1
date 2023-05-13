745319753e3ab9d8b9450eec795defd8ee719752
mdelvecchio <mdelvecchio3119@gmail.com>
2023-05-13 13:57:36 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- week3/w3d3_hw/hogwarts/app.js
+++ week3/w3d3_hw/hogwarts/app.js
@@ -16,3 +16,15 @@ $h1.text("Hogwarts");
 // console.log($h1)
 
 $container.append($h1);
+
+// Year 2
+
+const $name = $("<h2>").text("Michael");
+
+const $house = $("<h3>").text("Ravenclaw");
+
+const $pet = $("<h4>").addClass("owl").text("Chimble");
+
+const $wand = $("<h4>").text("Holly Wand with Unicorn Hair Core")
+
+$container.append($name, $house, $pet, $wand)
